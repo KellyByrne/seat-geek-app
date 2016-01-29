245a12065ccbb46e7974e6a81161f14922b3d35f
Kelly Byrne <kellyannbyrne@utexas.edu>
2016-01-28 22:29:12 -0600

    added performers and images of them

--- public/javascripts/controllers.js
+++ public/javascripts/controllers.js
@@ -1,5 +1,6 @@
 app.controller("SearchCtrl", ['$scope', '$http', function($scope, $http) {
   $scope.zeroResults = false;
+
   $scope.searchSeatGeek = function() {
     $http.get('https://api.seatgeek.com/2/events?q='+ $scope.queryData)
     .then(function(data){
@@ -18,10 +19,21 @@ app.controller("SearchCtrl", ['$scope', '$http', function($scope, $http) {
 
 app.controller("ShowCtrl", ['$scope', '$http', '$routeParams', function($scope, $http, $routeParams) {
   $scope.param1 = $routeParams.param1;
-  console.log($scope.param1)
+  $scope.performersExist = false;
+
   $http.get('https://api.seatgeek.com/2/events?id=' + $scope.param1).then(function(data){
     $scope.theEvent = data.data.events[0];
+    $scope.performers = data.data.events[0].performers;
+
+    if($scope.performers.length > 0){
+      $scope.performersExist = true;
+    } else if ($scope.performers.length === 0) {
+      $scope.performersExist = false;
+    }
+  
+
     console.log($scope.theEvent)
+    console.log($scope.performers);
   })
 
 }])
